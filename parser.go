1e071866d4f4f32a53618bcd888b0a4e74763e93
Arpit <arpit.tarang@gmail.com>
2013-06-24 15:47:45 +0530

    added data

--- parser.go
+++ parser.go
@@ -4,19 +4,41 @@ import (
 	"code.google.com/p/go.net/html"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"io/ioutil"
 	"os"
 	"strings"
 )
 
-func main() {
-	base, er := os.Getwd()
-	content, er := ioutil.ReadFile(base + "/data/source1.html")
-	if er != nil {
-		fmt.Println(er)
-		return
+func LoadDoc(page string) *goquery.Document {
+	base, _ := os.Getwd()
+	if file, e := os.Open(base + "/" + page); e != nil {
+		panic(e.Error())
+	} else {
+		defer file.Close()
+		if node, e := html.Parse(file); e != nil {
+			panic(e.Error())
+		} else {
+			return goquery.NewDocumentFromNode(node)
+		}
 	}
-	rawdoc, _ := html.Parse(strings.NewReader(string(content)))
-	doc := goquery.NewDocumentFromNode(rawdoc)
-	fmt.Println(doc.Text())
+	return nil
+}
+
+func IsSelected(str string) bool {
+	if strings.Contains(str, "Congratulations!") {
+		return true
+	}
+	return false
+}
+
+func IsInvalid(str string) bool {
+	if strings.Contains(str, "Roll Number is invalid") {
+		return true
+	}
+	return false
+}
+func main() {
+	doc := LoadDoc("data/gen.html")
+	doc2 := LoadDoc("data/invalid.html")
+
+	fmt.Println(IsSelected(doc.Text()), IsInvalid(doc.Text()), IsSelected(doc2.Text()), IsInvalid(doc2.Text()))
 }
--- query.go
+++ query.go
@@ -16,24 +16,25 @@ package main
 // 	if resp, err := http.PostForm("http://jee.iitd.ac.in/resultstatus.php",
 // 		url.Values{"regno": {strconv.Itoa(rollno)}, "submit": {"Submit"}}); err == nil {
 // 		defer resp.Body.Close()
-// 		/*body, _ :=*/ ioutil.ReadAll(resp.Body)
-// 		ch <- []byte(strconv.Itoa(rollno))
+// 		body, _ := ioutil.ReadAll(resp.Body)
+// 		ch <- body
 // 	} else {
 // 		ch <- nil
 // 	}
 // }
 
 // func main() {
-// 	ch := make(chan []byte, 1000)
-// 	for i := 0; i < 1000; i++ {
+// 	ch := make(chan []byte, 1)
+// 	for i := 0; i < 1; i++ {
 // 		go getResult(7023233+i, ch)
 // 		time.Sleep(3 * time.Millisecond)
 // 	}
 // 	for i := 0; i < cap(ch); i++ {
-// 		n, _ := strconv.ParseInt(string(<-ch), 10, 32)
-// 		if int(n-7023233) != i {
-// 			fmt.Println(n-7023233, i)
-// 		}
+// 		// n, _ := strconv.ParseInt(string(<-ch), 10, 32)
+// 		// if int(n-7023233) != i {
+// 		// 	fmt.Println(n-7023233, i)
+// 		// }
+//     fmt.Println(string(<-ch))
 // 		// fmt.Println(n-7023233, i)
 // 		// strconv.ParseInt(s, base, bitSize)
 // 	}
